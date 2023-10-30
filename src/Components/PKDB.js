f0910b055c44210392c1f539dd2267ffa87952b6
thbrthbr <tarzenezrat@gmail.com>
2023-10-30 20:49:52 +0900

    feat/imgDownload

--- src/Components/PKDB.js
+++ src/Components/PKDB.js
@@ -7,41 +7,108 @@ const categoryData = imageContext.keys().map(imageContext);
 
 let db = [
   {
+    name: 'Egg',
+    nameKo: '알',
+    code: '0000',
+    type: [],
+    url: '',
+  },
+  {
     name: 'Bulbasaur',
+    nameKo: '이상해씨',
     code: '0001',
     type: ['grass', 'poison'],
-    pos: [0, 128],
-    url: 'https://archives.bulbagarden.net/media/upload/7/70/Menu_HOME_0001.png',
+    url: '',
   },
   {
     name: 'Ivysaur',
+    nameKo: '이상해풀',
     code: '0002',
-    type: ['grass', 'posion'],
-    pos: [0, 256],
-    url: 'https://archives.bulbagarden.net/media/upload/b/b7/Menu_HOME_0002.png',
+    type: ['grass', 'poison'],
+    url: '',
   },
   {
     name: 'Venusaur',
+    nameKo: '이상해꽃',
     code: '0003',
     type: ['grass', 'poison'],
-    pos: [0, 384],
-    url: 'https://archives.bulbagarden.net/media/upload/9/99/Menu_HOME_0003.png',
+    url: '',
   },
   {
     name: 'Venusaur-Mega',
+    nameKo: '메가이상해꽃',
     code: '0003-m',
-    type: ['grass', 'posion'],
-    pos: [0, 512],
-    url: 'https://archives.bulbagarden.net/media/upload/0/0a/Menu_HOME_0003-Mega.png',
+    type: ['grass', 'poison'],
+    url: '',
   },
   {
     name: 'Charmander',
+    nameKo: '파이리',
     code: '0004',
     type: ['fire'],
-    pos: [0, 640],
-    url: 'https://archives.bulbagarden.net/media/upload/9/9c/Menu_HOME_0004.png',
+    url: '',
+  },
+  {
+    name: 'Charmeleon',
+    nameKo: '리자드',
+    code: '0005',
+    type: ['fire'],
+    url: '',
+  },
+  {
+    name: 'Charizard',
+    nameKo: '리자몽',
+    code: '0006',
+    type: ['fire', 'flying'],
+    url: '',
+  },
+  {
+    name: 'Charizard-Mega-X',
+    nameKo: '메가리자몽-X',
+    code: '0006-m1',
+    type: ['fire', 'dragon'],
+    url: '',
+  },
+  {
+    name: 'Charizard-Mega-Y',
+    nameKo: '메가리자몽-Y',
+    code: '0006-m2',
+    type: ['fire', 'flying'],
+    url: '',
+  },
+  {
+    name: 'Squirtle',
+    nameKo: '꼬부기',
+    code: '0007',
+    type: ['water'],
+    url: '',
+  },
+  {
+    name: 'Wartortle',
+    nameKo: '어니부기',
+    code: '0008',
+    type: ['water'],
+    url: '',
+  },
+  {
+    name: 'Blastoise',
+    nameKo: '거북왕',
+    code: '0009',
+    type: ['water'],
+    url: '',
+  },
+  {
+    name: 'Blastoise-Mega',
+    nameKo: '메가거북왕',
+    code: '0009-m',
+    type: ['water'],
+    url: '',
   },
 ];
+
+for (let i = 0; i < db.length; i++) {
+  db[i].url = categoryData[i];
+}
 
 // const PKDB = () => {
 //   let db = [
--- src/Components/PreferDiagram.js
+++ src/Components/PreferDiagram.js
@@ -1,17 +1,15 @@
 import styled from 'styled-components';
-
+import react, { useState, useRef } from 'react';
 import sprites from '../Default/spritesheet.png';
 import db from './PKDB';
+import { toPng } from 'html-to-image';
 
-const imageContext = require.context(
-  '../Default',
-  false,
-  /\.(jpg|jpeg|png|webp)$/,
-);
-const categoryData = imageContext.keys().map(imageContext);
-
-console.log(categoryData);
-console.log(categoryData[0]);
+// const imageContext = require.context(
+//   '../Default',
+//   false,
+//   /\.(jpg|jpeg|png|webp)$/,
+// );
+// const categoryData = imageContext.keys().map(imageContext);
 
 const $Container = styled.div`
   display: flex;
@@ -43,26 +41,239 @@ const $Card2 = styled.div`
 `;
 
 const PreferDiagram = () => {
+  const elementRef = useRef();
+
+  const [grass, setGrass] = useState(db[0].url);
+  const [water, setWater] = useState(db[0].url);
+  const [fire, setFire] = useState(db[0].url);
+  const [rock, setRock] = useState(db[0].url);
+  const [electric, setElectric] = useState(db[0].url);
+  const [ground, setGround] = useState(db[0].url);
+  const [dark, setDark] = useState(db[0].url);
+  const [fairy, setFairy] = useState(db[0].url);
+  const [fighting, setFighting] = useState(db[0].url);
+  const [psychic, setPsychic] = useState(db[0].url);
+  const [steel, setSteel] = useState(db[0].url);
+  const [bug, setBug] = useState(db[0].url);
+  const [poison, setPoison] = useState(db[0].url);
+  const [ghost, setGhost] = useState(db[0].url);
+  const [normal, setNormal] = useState(db[0].url);
+  const [ice, setIce] = useState(db[0].url);
+  const [dragon, setDragon] = useState(db[0].url);
+  const [flying, setFlying] = useState(db[0].url);
+
   const types = [
-    { type: '풀', color: '#008000', engType: 'grass', list: [] },
-    { type: '물', color: '#2771fb', engType: 'water', list: [] },
-    { type: '불꽃', color: '#ff0000', engType: 'fire', list: [] },
-    { type: '바위', color: '#cbb76f', engType: 'rock', list: [] },
-    { type: '전기', color: '#fffb00', engType: 'electric', list: [] },
-    { type: '땅', color: '#ffe8ac', engType: 'ground', list: [] },
-    { type: '악', color: '#4b4b4b', engType: 'dark', list: [] },
-    { type: '페어리', color: '#ffc0cb', engType: 'fairy', list: [] },
-    { type: '격투', color: '#8e0018', engType: 'fighting', list: [] },
-    { type: '에스퍼', color: '#ff109b', engType: 'psychic', list: [] },
-    { type: '강철', color: '#bed3d6', engType: 'steel', list: [] },
-    { type: '벌레', color: '#74d200', engType: 'bug', list: [] },
-    { type: '독', color: '#8f018a', engType: 'posion', list: [] },
-    { type: '고스트', color: '#480146', engType: 'ghost', list: [] },
-    { type: '노말', color: '#eeeeee', engType: 'normal', list: [] },
-    { type: '얼음', color: '#94faff', engType: 'ice', list: [] },
-    { type: '드래곤', color: '#3051ca', engType: 'dragon', list: [] },
-    { type: '비행', color: '#e7f9ff', engType: 'flying', list: [] },
+    {
+      type: '풀',
+      color: '#008000',
+      engType: 'grass',
+      list: [],
+      func: setGrass,
+      cur: grass,
+    },
+    {
+      type: '물',
+      color: '#2771fb',
+      engType: 'water',
+      list: [],
+      func: setWater,
+      cur: water,
+    },
+    {
+      type: '불꽃',
+      color: '#ff0000',
+      engType: 'fire',
+      list: [],
+      func: setFire,
+      cur: fire,
+    },
+    {
+      type: '바위',
+      color: '#cbb76f',
+      engType: 'rock',
+      list: [],
+      func: setRock,
+      cur: rock,
+    },
+    {
+      type: '전기',
+      color: '#fffb00',
+      engType: 'electric',
+      list: [],
+      func: setElectric,
+      cur: electric,
+    },
+    {
+      type: '땅',
+      color: '#ffe8ac',
+      engType: 'ground',
+      list: [],
+      func: setGround,
+      cur: ground,
+    },
+    {
+      type: '악',
+      color: '#4b4b4b',
+      engType: 'dark',
+      list: [],
+      func: setDark,
+      cur: dark,
+    },
+    {
+      type: '페어리',
+      color: '#ffc0cb',
+      engType: 'fairy',
+      list: [],
+      func: setFairy,
+      cur: fairy,
+    },
+    {
+      type: '격투',
+      color: '#8e0018',
+      engType: 'fighting',
+      list: [],
+      func: setFighting,
+      cur: fighting,
+    },
+    {
+      type: '에스퍼',
+      color: '#ff109b',
+      engType: 'psychic',
+      list: [],
+      func: setPsychic,
+      cur: psychic,
+    },
+    {
+      type: '강철',
+      color: '#bed3d6',
+      engType: 'steel',
+      list: [],
+      func: setSteel,
+      cur: steel,
+    },
+    {
+      type: '벌레',
+      color: '#74d200',
+      engType: 'bug',
+      list: [],
+      func: setBug,
+      cur: bug,
+    },
+    {
+      type: '독',
+      color: '#8f018a',
+      engType: 'posion',
+      list: [],
+      func: setPoison,
+      cur: poison,
+    },
+    {
+      type: '고스트',
+      color: '#480146',
+      engType: 'ghost',
+      list: [],
+      func: setGhost,
+      cur: ghost,
+    },
+    {
+      type: '노말',
+      color: '#eeeeee',
+      engType: 'normal',
+      list: [],
+      func: setNormal,
+      cur: normal,
+    },
+    {
+      type: '얼음',
+      color: '#94faff',
+      engType: 'ice',
+      list: [],
+      func: setIce,
+      cur: ice,
+    },
+    {
+      type: '드래곤',
+      color: '#3051ca',
+      engType: 'dragon',
+      list: [],
+      func: setDragon,
+      cur: dragon,
+    },
+    {
+      type: '비행',
+      color: '#e7f9ff',
+      engType: 'flying',
+      list: [],
+      func: setFlying,
+      cur: flying,
+    },
   ];
+
+  for (let i = 0; i < db.length; i++) {
+    if (db[i].type.includes('grass')) {
+      types[0].list.push(db[i]);
+    }
+    if (db[i].type.includes('water')) {
+      types[1].list.push(db[i]);
+    }
+    if (db[i].type.includes('fire')) {
+      types[2].list.push(db[i]);
+    }
+    if (db[i].type.includes('rock')) {
+      types[3].list.push(db[i]);
+    }
+    if (db[i].type.includes('electric')) {
+      types[4].list.push(db[i]);
+    }
+    if (db[i].type.includes('ground')) {
+      types[5].list.push(db[i]);
+    }
+    if (db[i].type.includes('dark')) {
+      types[6].list.push(db[i]);
+    }
+    if (db[i].type.includes('fairy')) {
+      types[7].list.push(db[i]);
+    }
+    if (db[i].type.includes('fighting')) {
+      types[8].list.push(db[i]);
+    }
+    if (db[i].type.includes('psychic')) {
+      types[9].list.push(db[i]);
+    }
+    if (db[i].type.includes('steel')) {
+      types[10].list.push(db[i]);
+    }
+    if (db[i].type.includes('bug')) {
+      types[11].list.push(db[i]);
+    }
+    if (db[i].type.includes('poison')) {
+      types[12].list.push(db[i]);
+    }
+    if (db[i].type.includes('ghost')) {
+      types[13].list.push(db[i]);
+    }
+    if (db[i].type.includes('normal')) {
+      types[14].list.push(db[i]);
+    }
+    if (db[i].type.includes('ice')) {
+      types[15].list.push(db[i]);
+    }
+    if (db[i].type.includes('dragon')) {
+      types[16].list.push(db[i]);
+    }
+    if (db[i].type.includes('flying')) {
+      types[17].list.push(db[i]);
+    }
+  }
+
+  const exportElementAsPNG = () => {
+    toPng(elementRef.current).then((image) => {
+      const link = window.document.createElement('a');
+      link.download = 'test.png';
+      link.href = image;
+      link.click();
+    });
+  };
 
   return (
     <>
@@ -70,8 +281,20 @@ const PreferDiagram = () => {
         {types.map((x) => {
           return (
             <$Card style={{ backgroundColor: x.color }}>
-              <select style={{ width: '90%' }}>
+              <select
+                style={{ width: '90%' }}
+                onChange={(e) => {
+                  for (let i = 0; i < x.list.length; i++) {
+                    if (x.list[i].name == e.target.value) {
+                      x.func(x.list[i].url);
+                    }
+                  }
+                }}
+              >
                 <option>{x.type}</option>
+                {x.list.map((y) => {
+                  return <option>{y.nameKo}</option>;
+                })}
               </select>
               <div
                 style={{
@@ -83,25 +306,19 @@ const PreferDiagram = () => {
               >
                 <img
                   style={{ width: '128px', height: '128px' }}
-                  src="https://archives.bulbagarden.net/media/upload/a/a0/Menu_HOME_Egg.png"
+                  src={x.cur}
                 ></img>
               </div>
             </$Card>
           );
         })}
       </$Container>
-      <div style={{ width: '128px', height: '128px', overflow: 'hidden' }}>
-        <img style={{ margin: '0px 0px 0px -512px' }} src={sprites}></img>
+      <div ref={elementRef}>
+        {db.map((x) => {
+          return <img src={x.url} />;
+        })}
       </div>
-      {/* <img
-        alt="failbaby"
-        src="https://archives.bulbagarden.net/media/upload/4/48/Menu_HOME_0999-Roaming.png"
-      ></img> */}
-      <img alt="failbaby" src={categoryData[0]}></img>
-      {/* {db.map((x) => {
-        return <img src={x.url} />;
-      })} */}
-      <$Diagram>
+      <$Diagram ref={elementRef}>
         {types.map((x) => {
           return (
             <$Card2 style={{ backgroundColor: x.color }}>
@@ -116,13 +333,14 @@ const PreferDiagram = () => {
               >
                 <img
                   style={{ width: '128px', height: '128px' }}
-                  src="https://archives.bulbagarden.net/media/upload/a/a0/Menu_HOME_Egg.png"
+                  src={x.cur}
                 ></img>
               </div>
             </$Card2>
           );
         })}
       </$Diagram>
+      <button onClick={exportElementAsPNG}>Download</button>
     </>
   );
 };
